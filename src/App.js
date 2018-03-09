11b49505cbc48496dbd0074e6d5c86c938168db4
Chris Reidy <chris.reidy@timeinc.com>
2018-03-09 18:09:05 +0000

    Call API and set state, once main component rendered

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import * as BooksAPI from './utils/BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
+  // Populate state's books data once API has been called
   state = {
+    books: []
+  }
+
+  // Once BooksApp component has rendered, call the Books API and update our app's state
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
   render() {
