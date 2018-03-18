fbaca700bcf0fc3f5c54464c068b931775330534
Chris Reidy <chris.reidy@timeinc.com>
2018-03-18 23:36:01 +0000

    Update comments for app and index js files

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
 import * as BooksAPI from './utils/BooksAPI'
-import './App.css'
 
 class BooksApp extends Component {
-  // Populate state's books data once API has been called
+  /*
+    * Populate state's book shelf books data once API has been called
+  */
   state = {
     books: []
   }
 
-  // Once BooksApp component has rendered, call the Books API and update our app's state
+  /*
+    * Once BooksApp component has rendered, call the Books API and update our app's state with books data
+  */
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
     })
+  }
+
+  /*
+    * Function is called when user has searched on a book and used the dropdown tool to add book to a shelf
+    * First call the Book API to update the API as to the new shelf the book has been moved to
+    * Then re-set our app's state, which will trigger a re-render.
+  */
+  addBook = (book, bookShelf) => {
   }
 
   render() {
@@ -24,11 +35,17 @@ class BooksApp extends Component {
 
         {/* Update UI based on the URL, using React Router's <Route> tag */}
         <Route exact path='/' render={() => (
-          <ListBooks/>
+          <ListBooks
+            onAddBook={this.addBook}
+            books={this.state.books}
+          />
         )}/>
 
         <Route path='/search' render={() => (
-          <SearchBooks/>
+          <SearchBooks
+            onAddBook={this.addBook}
+            books={this.state.books}
+          />
         )}/>
 
       </div>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
+import './App.css'
 import App from './App'
 
-/* 
+/*
 *	Wrapping my App in React Router's BrowserRouter component
-*	BrowserRouter listens to changes my App's URL and will notify other components when the URL does change
+*	BrowserRouter listens to changes in my App's URL and will notify other components when the URL does change
 */
 ReactDOM.render(
-	<BrowserRouter><App /></BrowserRouter>, 
+	<BrowserRouter><App /></BrowserRouter>,
 	document.getElementById('root')
 );
